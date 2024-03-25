c9eaceca61cf2c946b05b6455c299199fb826fb5
Prince Kumar Chaurasia <princechaurasiaofficial24@gmail.com>
2024-03-25 21:22:22 +0530

    changes made

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Product from "./components/Product";
 import Tasks from "./components/Tasks";
 import Prize from "./components/Prize";
 import Profile from "./components/Profile";
+import Team from "./components/pages/Team";
+import VIP from "./components/pages/VIP";
+import Reward from "./components/pages/Reward";
 
 function App() {
   return (
@@ -14,6 +17,9 @@ function App() {
         <Route path="/tasks" element={<Tasks />} />
         <Route path="/prize" element={<Prize />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/team" element={<Team/>} />
+        <Route path="/vip" element={<VIP />} />
+        <Route path="/reward" element={<Reward />} />
       </Routes>
     </Router>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,27 +8,29 @@ const Home = () => {
   return (
     <div className=" relative h-[100vh] bg-blue-600">
       <MenuBar />
-      <p className="px-6 py-3 text-3xl tracking-widest text-white font-bold">BMW</p>
+      <p className="px-6 py-3 text-3xl tracking-widest text-white font-bold">
+        BMW
+      </p>
       <div className="w-full p-2">
         <SliderComponent />
       </div>
       <div className="flex justify-between items-center mt-4 px-12">
-        <HashLink>
+        <HashLink to={"/telegram"}>
           <div className="buttons rounded-xl font-semibold text-xl bg-white w-72">
             Telegram
           </div>
         </HashLink>
-        <HashLink>
+        <HashLink to={"/reward"}>
           <div className="buttons rounded-xl font-semibold text-xl bg-white w-72">
             Reward
           </div>
         </HashLink>
-        <HashLink>
+        <HashLink to={"/vip"}>
           <div className="buttons rounded-xl font-semibold text-xl bg-white w-72">
             VIP
           </div>
         </HashLink>
-        <HashLink>
+        <HashLink to={"/team"}>
           <div className="buttons rounded-xl font-semibold text-xl bg-white w-72">
             Team
           </div>
